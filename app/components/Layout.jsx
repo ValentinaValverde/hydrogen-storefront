4ea0371c86ca1b648ac31de11e843d1bed7fde9d
Valentina Valverde <vvgreenville@gmail.com>
2023-11-07 11:45:57 -0500

    Commented out the issue (it was coming from Layout.jsx -> Footer.jsx). Commented out where Footer was being called, then a few minutes later while debugging un-commented it out and it started working again. I didn't have to touch anything in Footer.jsx. Strange.

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -21,11 +21,11 @@ export function Layout({cart, children = null, footer, header, isLoggedIn}) {
       <MobileMenuAside menu={header.menu} />
       <Header header={header} cart={cart} isLoggedIn={isLoggedIn} />
       <main>{children}</main>
-      {/* <Suspense>
+      <Suspense>
         <Await resolve={footer}>
           {(footer) => <Footer menu={footer.menu} />}
         </Await>
-      </Suspense> */}
+      </Suspense>
     </>
   );
 }
