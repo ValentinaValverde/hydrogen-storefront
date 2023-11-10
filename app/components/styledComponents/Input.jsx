799d373c9fc6d5f23e956fdd4bb6cd50637c00f9
Valentina Valverde <vvgreenville@gmail.com>
2023-11-10 08:04:41 -0500

    Saving work from yesterday.

---
+++ app/components/styledComponents/Input.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  width: 600px;
+  margin: 5px;
+`;
--- app/routes/custom_form._index.jsx
+++ app/routes/custom_form._index.jsx
@@ -1,8 +1,14 @@
 // GOAL(s):
-// get user information, including address
+// get user information
 // send user info to admin on submission
 
+import {useState} from 'react';
+import {Input} from '../components/styledComponents/Input';
+
 export default function CustomForm() {
+  const [productChoice, setProductChoice] = useState('');
+  const [sizeChoice, setSizeChoice] = useState('');
+
   const onClick = () => {
     console.log('clicked!!');
     const name_value = document.getElementById('name').value;
@@ -11,15 +17,19 @@ export default function CustomForm() {
     console.log(name_value, email_value, phoneNum_value);
   };
 
+  const onChange = (e) => {
+    setProductChoice(e.target.value);
+  };
+
   return (
     <>
       <fieldset className="customForm">
         <h1>Custom Form</h1>
         <h3>Contact Info</h3>
         <div className="contactCustomForm">
-          <input type="text" placeholder="Name" id="name" required />
-          <input type="email" placeholder="Email" id="email" required />
-          <input type="tel" placeholder="Phone Number" id="phoneNum" required />
+          <Input type="text" placeholder="Name" id="name" required />
+          <Input type="email" placeholder="Email" id="email" required />
+          <Input type="tel" placeholder="Phone Number" id="phoneNum" required />
         </div>
         <div className="customFormBottom">
           <h3>Product</h3>
@@ -27,36 +37,64 @@ export default function CustomForm() {
             <label>
               {' '}
               Please Select a Product &nbsp; &nbsp;
-              <select name="product" id="productOption">
+              <select name="product" value={productChoice} onChange={onChange}>
                 <option value="hoodie">Hoodie</option>
                 <option value="crewneck">Crewneck</option>
                 <option value="tee">Tee</option>
                 <option value="mug">Mug</option>
                 <option value="glassCan">Glass Can</option>
-                <option value="keychain">Keychain</option>
-                <option value="elf">Elf</option>
+                {/* <option value="keychain">Keychain</option> */}
+                {/* <option value="elf">Elf</option> */}
               </select>
             </label>
+
             <br />
             <label>
               Please Select a Size &nbsp; &nbsp;
-              <select name="size" id="sizeOption">
-                <option value="na">N/A</option>
-                <option value="small">Small</option>
-                <option value="medium">Medium</option>
-                <option value="large">Large</option>
-                <option value="XL">XL</option>
-                <option value="2XL">2XL</option>
-                <option value="3XL">3XL</option>
-                <option value="16">16oz</option>
-                <option value="20">20oz</option>
+              <select
+                id="apparelSize"
+                value={sizeChoice}
+                onChange={(e) => setSizeChoice(e.target.value)}
+              >
+                {productChoice == 'hoodie' ||
+                productChoice == 'crewneck' ||
+                productChoice == 'tee' ? (
+                  <>
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                    <option value="XL">XL</option>
+                    <option value="2XL">2XL</option>
+                    <option value="3XL">3XL</option>
+                  </>
+                ) : (
+                  <>
+                    <option value="none">N/A</option>
+                    <option value="16">16oz</option>
+                    <option value="20">20oz</option>
+                  </>
+                )}
               </select>
             </label>
           </div>
-          <input type="text" placeholder="Material Color" />
-          <small>*if applicable</small>
-          <input type="text" placeholder="Vinyl Color" />
-          <input type="text" placeholder="Any other information" />
+
+          <div>
+            {productChoice == 'hoodie' ||
+            productChoice == 'crewneck' ||
+            productChoice == 'tee' ? (
+              <>
+                <Input type="text" placeholder="Material Color" />
+              </>
+            ) : (
+              <>
+                <p></p>
+              </>
+            )}
+          </div>
+          {/* <small>*if applicable</small> */}
+
+          <Input type="text" placeholder="Vinyl Color" />
+          <Input type="text" placeholder="Any other information" />
           <button type="submit" onClick={onClick}>
             Submit
           </button>
@@ -73,3 +111,14 @@ export default function CustomForm() {
 //size (if applicable)
 //color - text
 //vinyl color
+
+// {productChoice == 'glassCan' ? (
+//   <>
+//     <option value="16">16oz</option>
+//     <option value="20">20oz</option>
+//   </>
+// ) : (
+//   <>
+//     <option>none</option>
+//   </>
+// )}
